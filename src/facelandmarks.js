1a10bfe89d73c14242d358b2ba4baed46068462e
KoT9r <popeskudenis@gmail.com>
2020-03-04 16:07:43 +0300

    Added definition of the lip area

--- src/facelandmarks.js
+++ src/facelandmarks.js
@@ -1,7 +1,6 @@
-import * as faceapi from 'face-api.js'; 
+import * as faceapi from 'face-api.js';
 import * as React from 'react';
 import man from './man.jpg';
-import lips from './lips.jpg';
 import './hidden.css';
 
 
@@ -16,24 +15,45 @@ export class FaceLandmarks extends React.Component {
     const canvas = this.refs.canvas
     const ctx = canvas.getContext("2d")
     let img = document.getElementById(this.face_id)
-    
+
     const displaySize = { width: img.width, height: img.height }
     faceapi.matchDimensions(canvas, displaySize)
 
     ctx.drawImage(img, 0, 0)
-
 
     let landmarks = await detect(img.id)
     const resizedResults = faceapi.resizeResults(landmarks, displaySize)
     faceapi.draw.drawDetections(canvas, resizedResults)
     faceapi.draw.drawFaceLandmarks(canvas, resizedResults)
 
-    ctx.drawImage(document.getElementById("lips"), landmarks[0].landmarks.positions[30].x, landmarks[0].landmarks.positions[30].y)
+    const upperLip = landmarks[0].landmarks.positions.slice(48, 55)
+        .concat(landmarks[0].landmarks.positions.slice(60, 65).reverse())
+    const lowerLip = landmarks[0].landmarks.positions.slice(54, 60)
+        .concat([landmarks[0].landmarks.positions[48]])
+        .concat([landmarks[0].landmarks.positions[60]])
+        .concat(landmarks[0].landmarks.positions.slice(64, 68).reverse())
+
+    ctx.fillStyle = '#aaa';
+    ctx.beginPath()
+    ctx.moveTo(upperLip[0].x, upperLip[0].y)
+    for (let i=1; i < upperLip.length-1; i += 1) {
+      ctx.lineTo(upperLip[i].x, upperLip[i].y)
+    }
+    ctx.closePath()
+    ctx.fill()
+
+    ctx.beginPath()
+    ctx.moveTo(lowerLip[0].x, lowerLip[0].y)
+    for (let i=1; i < lowerLip.length-1; i += 1) {
+      ctx.lineTo(lowerLip[i].x, lowerLip[i].y)
+    }
+    ctx.closePath()
+    ctx.fill()
   }
-  
+
   async componentDidMount() {
     await loadModels()
-    await this.findFaceLandmarks() 
+    await this.findFaceLandmarks()
   }
 
   async updateImage(newImg) {
@@ -44,31 +64,30 @@ export class FaceLandmarks extends React.Component {
 
   render() {
     return(
-      <div>
-        <canvas ref="canvas" width={0} height={0} />
-        <img id={this.face_id} ref="face" src={man} className="hidden" alt='Man face'/>
-        <img id="lips" src={lips} className="hidden" alt='lips'/>
-      </div>
+        <div>
+          <canvas ref="canvas" width={0} height={0} />
+          <img id={this.face_id} ref="face" src={man} className="hidden" alt='Man face'/>
+        </div>
     )
   }
 }
 
 async function loadModels()
 {
-    const MODEL_URL = process.env.PUBLIC_URL + '/models';
-    console.log(MODEL_URL)
-    await Promise.all([
-      faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL),
-      faceapi.nets.faceLandmark68TinyNet.loadFromUri(MODEL_URL),
-    ]).catch(error => {
-      console.error(error)
-    })
+  const MODEL_URL = process.env.PUBLIC_URL + '/models';
+  console.log(MODEL_URL)
+  await Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL),
+    faceapi.nets.faceLandmark68TinyNet.loadFromUri(MODEL_URL),
+  ]).catch(error => {
+    console.error(error)
+  })
 }
 
 export async function detect(img_id)
 {
-    return faceapi.detectAllFaces(img_id, new faceapi.TinyFaceDetectorOptions())
-        .withFaceLandmarks(true)
+  return faceapi.detectAllFaces(img_id, new faceapi.TinyFaceDetectorOptions())
+      .withFaceLandmarks(true)
 }
 
 function get(object, key, default_value) {
