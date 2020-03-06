a9d4edbb9eec680780c8a9c796004792d8de3417
Даниил Агалаков <61158013+AgalakovDV@users.noreply.github.com>
2020-03-06 23:32:16 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,37 +1,53 @@
 import React from 'react';
 import './App.css';
 
-import {FaceLandmarks} from './facelandmarks';
+import man from './man.jpg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header" id="container">
-       <form encType="multipart/form-data" method="post">
-              <p>
-               <b>
-                <i> Загрузите фотографию </i>
-               </b>
-              </p>
-              <p>
-                  <input
-                      type="file"
-                      name="photo"
-                      multiple accept="image/*,image/jpeg"
-                  />
-                  <input
-                      type="submit"
-                      value="Отправить"
-                  />
-              </p>              
+import { LipsChanger } from './LipsChanger';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.img = get(props, 'src', man)
+    this.color = get(props, 'color', '#f00')
+    this.lipsChangerRef = 'changer'
+
+  }
+
+  async changeImage(e) {
+    const file = e.target.files[0];
+    this.refs[this.lipsChangerRef].updateImage(URL.createObjectURL(file))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header" id="container">
+         <form encType="multipart/form-data" method="post">
+                <p><b><i> Загрузите фотографию </i></b></p>
+                <p>
+                    <input
+                        type="file"
+                        name="photo"
+                        multiple accept="image/*,image/jpeg"
+                        onChange={this.changeImage.bind(this)}
+                    />
+                </p>              
           </form>
-        <FaceLandmarks />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
-  );
+          <LipsChanger ref={this.lipsChangerRef} src={this.img} color={this.color}/>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+
+function get(object, key, default_value) {
+  var result = object[key];
+  return (typeof result !== "undefined") ? result : default_value;
 }
 
 export default App;
